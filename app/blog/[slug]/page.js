0baf566a7ac586e620fbc2e220e47fc0eb795b50
evanmarshall-dev <me@evanmarshall.dev>
2024-09-19 13:41:58 -0300

    BLOG: Added new hero image. Styled hero and blog main images responsively. Added some custom styles for the blogs into globals.css. Updated padding and margin on SearchBar.

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,6 +2,7 @@ import Markdown from "markdown-to-jsx";
 import getPostMetadata from "@/utils/getPostMetadata";
 import matter from "gray-matter";
 import fs from "fs";
+import Image from "next/image";
 
 function getPostContent(slug) {
   const folder = "posts/";
@@ -9,7 +10,11 @@ function getPostContent(slug) {
   const content = fs.readFileSync(file, "utf-8");
 
   const matterResult = matter(content);
-  return matterResult;
+  // ? return matterResult;
+  return {
+    content: matterResult.content,
+    data: matterResult.data,
+  };
 }
 
 export const generateStaticParams = async () => {
@@ -26,7 +31,8 @@ export async function generateMetadata({ params }) {
 
 export default function BlogPage(props) {
   const slug = props.params.slug;
-  const post = getPostContent(slug);
+  // ? const post = getPostContent(slug);
+  const { content, data } = getPostContent(slug);
   const options = {
     overrides: {
       h1: {
@@ -35,12 +41,63 @@ export default function BlogPage(props) {
           className: "custom-h1",
         },
       },
+      h2: {
+        component: "h2",
+        props: {
+          className: "custom-h2",
+        },
+      },
+      h3: {
+        component: "h3",
+        props: {
+          className: "custom-h3",
+        },
+      },
+      img: {
+        component: "img",
+        props: {
+          className: "custom-img",
+        },
+      },
+      p: {
+        component: "p",
+        props: {
+          className: "custom-p",
+        },
+      },
+      em: {
+        component: "em",
+        props: {
+          className: "custom-em",
+        },
+      },
+      hr: {
+        component: "hr",
+        props: {
+          className: "custom-hr",
+        },
+      },
     },
   };
 
   return (
-    <article className="pt-16">
-      <Markdown options={options}>{post.content}</Markdown>
+    <article>
+      <div className="grid grid-cols-1 gap-2 mt-16 mb-8 mx-auto md:mx-8">
+        <div className="relative h-[480px]">
+          <Image
+            src={data.cover_image}
+            alt={`Main image for blog: ${data.title}`}
+            fill
+            sizes="(min-width: 768px) 50vw, 100vw"
+            className="object-contain object-center"
+          />
+        </div>
+      </div>
+      <h2 className="mb-2 text-xl font-extrabold text-gray-900 md:text-2xl lg:text-3xl">
+        {data.title}
+      </h2>
+      <p className="mb-8 font-bold">{data.date}</p>
+      <Markdown options={options}>{content}</Markdown>
     </article>
   );
 }
--- app/ui/Hero.js
+++ app/ui/Hero.js
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <>
-      <Image
-        src="/beach-bums.jpg"
-        height={850}
-        width={850}
-        alt="Isla and Rae sitting in front of the ocean water at a beach"
-        className="mx-auto pt-16"
-      />
-    </>
+    <div className="grid grid-cols-1 gap-2 mt-16 mx-auto md:mx-8">
+      <div className="relative h-[600px]">
+        <Image
+          src="/family.jpg"
+          alt="Photo of the Marshall family in the grass at the golden hour"
+          fill
+          sizes="(min-width: 768px) 50vw, 100vw"
+          className="object-cover object-center"
+        />
+      </div>
+    </div>
   );
 }
